44919110f7f340c5d2af6c7c6fdbc615483e9bbf
andy-kuzn <114844265+andy-kuzn@users.noreply.github.com>
2023-01-28 14:29:03 +0900

    Add files via upload

---
+++ test_main.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello, world!"}
+
+def test_predict_translation():
+    response = client.post("/predict/",
+        json={"text": "I hate machine learning!"}
+    )
+    json_data = response.json() 
+
+    assert response.status_code == 200
+    assert response.json() == {"translation_text": "Ненавижу машинное обучение!"}
